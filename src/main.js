5436839426dda28782e9689cdf922d6fa085335f
Nursyafriady Yadi <nursyafriady83@gmail.com>
2021-09-08 20:54:04 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+//import router
+import router from './router/index'
+
+//import axios
+import axios from 'axios'
+
+//set credential axios with true
+axios.defaults.withCredentials = true
 
 Vue.config.productionTip = false
 
+//import bootstrap css & js
+import 'bootstrap/dist/css/bootstrap.css'
+import 'jquery/dist/jquery.min'
+import 'popper.js/dist/popper.min'
+import 'bootstrap/dist/js/bootstrap.min'
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
